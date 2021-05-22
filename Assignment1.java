13773a45a20d12f01e9485c515f1b7f59278080c
jimmyFallon <brandon.staley@protonmail.com>
2021-05-22 09:17:30 -0500

    An assignment around printing patterns

---
+++ Assignment1.java
@@ -0,0 +1,29 @@
+public class Assignment1 {
+    //Print out a set of patters.
+    
+
+    //p# is the part number specified by the assignment details
+    public static void p1()
+    {
+        System.out.println("1)\n*\n**\n***\n****\n.........");
+    }
+    public static void p2()
+    {
+        System.out.println("2)\n...........\n****\n***\n**\n*");
+    }
+    public static void p3()
+    {
+        System.out.println("3)\n     *\n    ***\n   *****\n  *******\n..........");
+    }
+    public static void p4()
+    {
+        System.out.println("4)\n..........\n  *******\n   *****\n    ***\n     *");
+    }
+
+    public static void main(String[] args) {
+        p1();
+        p2();
+        p3();
+        p4();
+    }
+}
